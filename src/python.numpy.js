1840861f55ef125e81c87454fb8f012da789944f
Ganesh K Bhat <ganeshsurfs@gmail.com>
2023-04-29 19:49:13 +0530

    [COMMIT] Adding node-gyp as a preinstall requirement

---
+++ src/python.numpy.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ * Package: ndarrays
+ * Author: Ganesh B
+ * Description: 
+ * Install: npm i ndarrays --save
+ * Github: https://github.com/ganeshkbhat/numericalarrays
+ * npmjs Link: https://www.npmjs.com/package/ndarrays
+ * File: index.mjs
+ * File Description: 
+ * 
+ * 
+ * 
+ * References:
+ * 
+ * Covers Numpy Modules and Files:
+ * 
+ * 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
+
+
